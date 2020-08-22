826a4b7bab0387f92718b66731a975d154a917e4
MuratovArthur <arturlove45@gmail.com>
2020-08-22 16:16:16 +0600

    Style update

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -15,7 +15,7 @@
 
     if (window.matchMedia("(max-width: 500px)").matches) {
       document.getElementById("interface").innerHTML = "Используем удобный и понятный интерфейс";
-      document.querySelector(".featurette-heading-last").innerHTML = "Обучение и развитие";
+      document.querySelector(".a-last").innerHTML = "Обучение и развитие";
     }
 
     if (window.matchMedia("(max-width: 450px)").matches) {
