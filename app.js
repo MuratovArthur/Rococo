9fa4ca9b9e922773c1dcb9301ce574ca81a96ea3
MuratovArthur <arturlove45@gmail.com>
2020-08-22 16:07:58 +0600

    Update for Heroku

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.get("/study", function (req, res){
 });
 
 
-app.listen(3000, function() { 
-  console.log("Server listening on port 3000"); 
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
 });
 
