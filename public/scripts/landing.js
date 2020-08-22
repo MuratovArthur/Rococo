d7fea22f61521873c64a9328dad5c7dad917d70d
MuratovArthur <arturlove45@gmail.com>
2020-08-22 17:06:52 +0600

    Update landing.js

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -2,7 +2,7 @@
     window.addEventListener('resize', function() {
       if (window.matchMedia("(max-width: 500px)").matches) {
         document.getElementById("interface").innerHTML = "Используем удобный и понятный интерфейс";
-        document.querySelector(".featurette-heading-last").innerHTML = "Обучение и развитие";
+      document.querySelector(".a-last").innerHTML = "Обучение и развитие";
       };
       if (window.matchMedia("(max-width: 450px)").matches) {
         document.getElementById("interface").innerHTML = "Используем удобный интерфейс";
