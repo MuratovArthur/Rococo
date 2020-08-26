3bffe01e01c23d6c2d90411111c2f0cb82a51cba
MuratovArthur <arturlove45@gmail.com>
2020-08-26 16:51:07 +0600

    Database v1.0

--- app.js
+++ app.js
@@ -1,24 +1,76 @@
-var express = require("express"),
-    ejs     = require('ejs'),
-    app     = express();
+var express    = require("express"),
+    ejs        = require('ejs'),
+    mongoose   = require("mongoose"),
+    bodyParser = require("body-parser"), 
+    app        = express();
 
+mongoose.connect("mongodb://localhost/rococo", {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+var foodSchema = new mongoose.Schema({
+	image: String,
+	title: String,
+	subtitle: String,
+	description: String,
+	extra: String
+});
+
+var Food = mongoose.model("Food", foodSchema);
+
+var filmSchema = new mongoose.Schema({
+	image: String,
+	title: String,
+	subtitle: String,
+	description: String,
+	link: String
+});
+
+var Film = mongoose.model("Film", filmSchema);
+
+var studySchema = new mongoose.Schema({
+	image: String,
+	title: String,
+	subtitle: String,
+	description: String,
+	link: String
+});
+
+var Study = mongoose.model("Study", studySchema);
 
 app.get("/", function (req, res){
 	res.render("landing");
 });
 
 app.get("/food", function (req, res){
-  res.render("food");
+  Food.find({}, function(err, foodCards){
+		if (err){
+           console.log(err);
+		}else{
+            res.render("food", {foodCards: foodCards});
+		}
+	})
 });
 
 app.get("/films", function (req, res){
-  res.render("films");
+  Film.find({}, function(err, filmCards){
+		if (err){
+           console.log(err);
+		}else{
+            res.render("films", {filmCards: filmCards});
+		}
+	})
 });
 
 app.get("/study", function (req, res){
-  res.render("study");
+  Study.find({}, function(err, studyCards){
+		if (err){
+           console.log(err);
+		}else{
+            res.render("study", {studyCards: studyCards});
+		}
+	})
 });
 
 
