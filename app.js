894500a1e1e88cdc1a53e2c4fe2ecd60b8ea8ce6
MuratovArthur <arturlove45@gmail.com>
2020-08-27 22:46:03 +0600

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ AdminBro.registerAdapter(AdminBroMongoose)
 
 var urlencoded = bodyParser.urlencoded({ extended: true }) ;
 
-app.use(bodyParser.json());
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(session({ 
